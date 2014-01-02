f71bf2d04ea35c686ceea5f0e687968cabf8c1c8
John Nadeau <johnnnadeau@gmail.com>
2014-01-02 15:47:36 -0500

    APIClient Coverage

--- AlertSolutions.APIClient.Test/ApiClientTest.cs
+++ AlertSolutions.APIClient.Test/ApiClientTest.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using AlertSolutions.API;
+using AlertSolutions.API.Broadcasts;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using Moq;
 
@@ -16,6 +17,9 @@ namespace AlertSolutions.APIClient.Test
         public void ConstructorTest()
         {
             ApiClient apiClient = new ApiClient();
+            Assert.IsNotNull(apiClient);
+
+            apiClient = new ApiClient("url", "user", "password");
             Assert.IsNotNull(apiClient);
         }
 
@@ -66,23 +70,53 @@ namespace AlertSolutions.APIClient.Test
         }
 
         [TestMethod]
-        public void Test()
+        public void CancelOrderTest()
         {
             var mockWebClientProxy = new Mock<IWebClientProxy>();
-            mockWebClientProxy.Setup(x => x.DownloadString(It.IsAny<string>(), It.IsAny<string>())).Returns(() =>
-            {
-                return "downloaded string";
-            });
             mockWebClientProxy.Setup(x => x.UploadString(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
                 .Returns(
                     () => { return "some result"; });
             ApiClientResolver.Instance.Container.Register(mockWebClientProxy.Object);
             ApiClient apiClient = new ApiClient();
             apiClient.Initialize("blah", "user", "password");
-            string retval = apiClient.CancelOrder(1, OrderType.VoiceBroadcast);
-            Assert.IsNotNull("some result");
+            string retval = apiClient.CancelOrder(new OrderResponse{OrderID = 1, OrderType = OrderType.EmailBroadcast});
+            Assert.AreEqual("some result", retval);
+        }
 
+        [TestMethod]
+        public void LaunchBroadcastTest()
+        {
+            var mockWebClientProxy = new Mock<IWebClientProxy>();
+            mockWebClientProxy.Setup(x => x.UploadString(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(
+                    () => { return "<PostAPIResponse><SaveTransactionalOrderResult><OrderID>12345</OrderID></SaveTransactionalOrderResult></PostAPIResponse>"; });
+            ApiClientResolver.Instance.Container.Register(mockWebClientProxy.Object);
 
+            var mockBroadcast = new Mock<IBroadcast>();
+            mockBroadcast.Setup(x => x.ToXml()).Returns("<Orders><Order Type='EB'></Order></Orders>");
+
+            ApiClient apiClient = new ApiClient("url", "user", "password");
+            OrderResponse response = apiClient.LaunchBroadcast(mockBroadcast.Object);
+            Assert.AreEqual(RequestResultType.Success, response.RequestResult);
+            Assert.AreEqual(OrderType.EmailBroadcast, response.OrderType);
+            Assert.AreEqual(12345, response.OrderID);
+        }
+
+        [TestMethod]
+        public void GetOrderReportTest()
+        {
+            var mockWebClientProxy = new Mock<IWebClientProxy>();
+            mockWebClientProxy.Setup(x => x.UploadString(It.IsAny<string>(), It.IsAny<string>(), It.IsAny<string>()))
+                .Returns(
+                    () => { return "<PostAPIResponse><SaveTransactionalOrderResult><status>status</status></SaveTransactionalOrderResult></PostAPIResponse>"; });
+            ApiClientResolver.Instance.Container.Register(mockWebClientProxy.Object);
+
+            ApiClient apiClient = new ApiClient("url", "user", "password");
+            OrderReport response = apiClient.GetOrderReport(12345, OrderType.EmailBroadcast, ReportReturnType.XML);
+            Assert.AreEqual(RequestResultType.Success, response.RequestResult);
+            Assert.AreEqual(OrderType.EmailBroadcast, response.OrderType);
+            Assert.AreEqual(12345, response.OrderID);
+            Assert.AreEqual("<PostAPIResponse><SaveTransactionalOrderResult><status>status</status></SaveTransactionalOrderResult></PostAPIResponse>", response.ReportData);         
         }
 
         [TestCleanup]

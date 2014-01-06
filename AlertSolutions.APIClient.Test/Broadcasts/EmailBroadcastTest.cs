16cb819d42feb30bcca245ddd1f070ef80eda0b1
James Thomas <jthomas@alertsolutions.com>
2014-01-06 18:08:29 -0500

    APIClient Test Coverage

--- AlertSolutions.APIClient.Test/Broadcasts/EmailBroadcastTest.cs
+++ AlertSolutions.APIClient.Test/Broadcasts/EmailBroadcastTest.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 using AlertSolutions.API.Broadcasts;
 using AlertSolutions.API.Documents;
@@ -24,32 +25,195 @@ namespace AlertSolutions.APIClient.Test.Broadcasts
             Assert.IsNotNull(emailBroadcast);
         }
 
-        [TestMethod]
-        public void XmlOutputTest()
-        {
-            string attachmentFile = DocumentSamples.GetSampleCsv();
-            string textbody = DocumentSamples.GetSampleEmailText();
-            string htmlbody = DocumentSamples.GetSampleEmailHtml();
-            string contactlist = DocumentSamples.GetSampleContactListCsv();
+        // msbuild doesn't seem to handle this attribute, so it fails testfast.bat
+        //[TestMethod, ExpectedException(typeof(FormatException))]
+        //public void BuildXmlWithoutRequiredInput()
+        //{
+        //    var eb = new EmailBroadcast();
+        //    var xml = eb.ToXml();
+        //}
 
+        [TestMethod]
+        public void BuildXmlWithRequiredInput()
+        {
             var eb = new EmailBroadcast();
-            eb.BillCode = "PostAPIClient Refactor Test";
-            eb.ProjectCode = "PostAPIClient Refactor Test";
-            eb.EmailSubject = "PostAPIClient Refactor Test (EB)";
-            eb.EmailReplyTo = "jthomas@blimessaging.com";
-            eb.EmailFrom = "jthomas@blimessaging.com";
-            eb.DisplayName = "JThomas from AlertSolutions";
-            eb.Attachments = new List<Attachment>() { Attachment.FromText("file.txt", attachmentFile) };
-            eb.TextBody = TextBody.FromText(textbody);
-            eb.HtmlBody = HtmlBody.FromText(htmlbody);
-            eb.List = ContactList.FromText("ContactList.csv", contactlist);
-            eb.EmailHeader = "email";
-            eb.Proofs = new List<string>() { "jthomas@blimessaging.com" };
-            Assert.IsNotNull(eb.ToXml());
-            Assert.IsNotNull(eb.ResendInterval);
+            eb.TextBody = TextBody.FromText("Required content");
+            eb.List = ContactList.FromText("requiredList.csv","email\r\nsanta@northpole.com");
+            
+            var xml = eb.ToXml();
+            Assert.IsNotNull(xml);
+            XDocument xDoc = XDocument.Parse(xml);
+            Assert.IsNotNull(xDoc);
         }
 
-        [TestMethod, Ignore]
+
+        [TestMethod]
+        public void XmlOutputAllFieldsTest()
+        {
+            var eb = new EmailBroadcast();
+            eb.BillCode = "BC";
+            eb.ProjectCode = "PC";
+            eb.EmailSubject = "ES";
+            eb.EmailReplyTo = "ERT";
+            eb.EmailFrom = "EF";
+            eb.DisplayName = "DN";
+            eb.List = ContactList.FromText("requiredList.csv", "email\r\nsanta@northpole.com");
+            eb.EmailHeader = "email";
+            // one of these must be populated, either text, html, or both
+            eb.TextBody = TextBody.FromText("Required content");
+            eb.HtmlBody = HtmlBody.FromText("Required content");
+            
+            var xml = eb.ToXml();
+            Assert.IsNotNull(xml);
+            XDocument xDoc = XDocument.Parse(xml);
+            Assert.IsNotNull(xDoc);
+
+            // make sure the necessary email fields are there (as per postAPI documentation)
+            VerifyFieldsExist(xDoc);
+
+            // make sure the necessary VALUES exist in those fields (as per postAPI documentation)
+            // TODO : make match values set in object
+            var orderTag = xDoc.Root.Elements("Order").FirstOrDefault();
+            Assert.IsTrue(orderTag.FirstAttribute.Value == "EB");
+            Assert.IsNotNull(orderTag.Element("Project"));
+            Assert.IsNotNull(orderTag.Element("BillCode"));
+            Assert.IsNotNull(orderTag.Element("AutoLaunch"));
+            Assert.IsNotNull(orderTag.Element("Date"));
+            Assert.IsNotNull(orderTag.Element("Time"));
+            Assert.IsNotNull(orderTag.Element("DisplayName"));
+            Assert.IsNotNull(orderTag.Element("From"));
+            Assert.IsNotNull(orderTag.Element("ReplyTo"));
+            Assert.IsNotNull(orderTag.Element("Subject"));
+            Assert.IsNotNull(orderTag.Element("Forward"));
+            Assert.IsNotNull(orderTag.Element("ReplaceLink"));
+            Assert.IsNotNull(orderTag.Element("Unsubscribe"));
+            Assert.IsNotNull(orderTag.Element("NumberOfRedials"));
+            Assert.IsNotNull(orderTag.Element("NumberOfResends"));
+
+            // TODO : make tests for these conditional fields and their output
+            // conditionals
+            Assert.IsNotNull(orderTag.Element("ResendInterval"));
+            Assert.IsNotNull(orderTag.Element("ListID"));
+            Assert.IsNotNull(orderTag.Element("ListName"));
+            Assert.IsNotNull(orderTag.Element("ListBinary"));
+            Assert.IsNotNull(orderTag.Element("EmailField")); // header <- what happens when not set?
+
+            // documents
+            Assert.IsNotNull(orderTag.Element("HtmlID"));
+            Assert.IsNotNull(orderTag.Element("HtmlFile"));
+            Assert.IsNotNull(orderTag.Element("HtmlBinary"));
+            Assert.IsNotNull(orderTag.Element("TextID"));
+            Assert.IsNotNull(orderTag.Element("TextFile"));
+            Assert.IsNotNull(orderTag.Element("TextBinary"));
+
+            // lists
+            Assert.IsNotNull(orderTag.Element("Proofs").Elements("Proof").Any());
+            Assert.IsNotNull(orderTag.Element("Attachments"));
+            Assert.IsTrue(orderTag.Element("Attachments").Elements().Any());
+        }
+
+        public void VerifyFieldsExist(XDocument orderXml)
+        {
+            Assert.IsNotNull(orderXml);
+            var orderTag = orderXml.Root.Elements("Order").FirstOrDefault();
+            Assert.IsTrue(orderTag.FirstAttribute.Value == "EB");
+            Assert.IsNotNull(orderTag.Element("Project"));
+            Assert.IsNotNull(orderTag.Element("BillCode"));
+            Assert.IsNotNull(orderTag.Element("AutoLaunch"));
+            Assert.IsNotNull(orderTag.Element("Date"));
+            Assert.IsNotNull(orderTag.Element("Time"));
+            Assert.IsNotNull(orderTag.Element("DisplayName"));
+            Assert.IsNotNull(orderTag.Element("From"));
+            Assert.IsNotNull(orderTag.Element("ReplyTo"));
+            Assert.IsNotNull(orderTag.Element("Subject"));
+            Assert.IsNotNull(orderTag.Element("Forward"));
+            Assert.IsNotNull(orderTag.Element("ReplaceLink"));
+            Assert.IsNotNull(orderTag.Element("Unsubscribe"));
+            Assert.IsNotNull(orderTag.Element("NumberOfRedials"));
+            Assert.IsNotNull(orderTag.Element("NumberOfResends"));
+            Assert.IsNotNull(orderTag.Element("ResendInterval"));
+            Assert.IsNotNull(orderTag.Element("ListID"));
+            Assert.IsNotNull(orderTag.Element("ListName"));
+            Assert.IsNotNull(orderTag.Element("ListBinary"));
+            Assert.IsNotNull(orderTag.Element("EmailField"));
+            Assert.IsNotNull(orderTag.Element("HtmlID"));
+            Assert.IsNotNull(orderTag.Element("HtmlFile"));
+            Assert.IsNotNull(orderTag.Element("HtmlBinary"));
+            Assert.IsNotNull(orderTag.Element("TextID"));
+            Assert.IsNotNull(orderTag.Element("TextFile"));
+            Assert.IsNotNull(orderTag.Element("TextBinary"));
+            Assert.IsNotNull(orderTag.Element("Proofs").Elements("Proof").Any());
+            Assert.IsNotNull(orderTag.Element("Attachments"));
+            Assert.IsTrue(orderTag.Element("Attachments").Elements().Any());
+        }
+
+        [TestMethod]
+        public void XmlOutputProofTest()
+        {
+            var eb = new EmailBroadcast();
+            eb.TextBody = TextBody.FromText("required content");
+            eb.List = ContactList.FromText("requiredList.csv", "email\r\nsanta@northpole.com");
+            eb.Proofs = new List<string>() { "elf1@northpole.com", "elf2@northpole.com", "elf3@northpole.com", "elf4@northpole.com", "elf5@northpole.com" };
+
+            var xml = eb.ToXml();
+            XDocument xDoc = XDocument.Parse(xml);
+
+            var orderTag = xDoc.Root.Elements("Order").FirstOrDefault();
+            var proofs = orderTag.Element("Proofs").Elements().ToList();
+            Assert.IsTrue(orderTag.Element("Proofs").Elements().Count() ==  5);
+            Assert.IsTrue(proofs[0].Value == "elf1@northpole.com");
+            Assert.IsTrue(proofs[1].Value == "elf2@northpole.com");
+            Assert.IsTrue(proofs[2].Value == "elf3@northpole.com");
+            Assert.IsTrue(proofs[3].Value == "elf4@northpole.com");
+            Assert.IsTrue(proofs[4].Value == "elf5@northpole.com");
+
+            // overloading the amount of allowable proofs
+            eb.Proofs.Add("elf6@northpole.com");
+
+            xml = eb.ToXml();
+            xDoc = XDocument.Parse(xml);
+            orderTag = xDoc.Root.Elements("Order").FirstOrDefault();
+            Assert.IsTrue(orderTag.Element("Proofs").Elements().Count() == 6);
+        }
+
+        [TestMethod]
+        public void XmlOutputAttachmentTest()
+        {
+            var eb = new EmailBroadcast();
+            eb.TextBody = TextBody.FromText("required content");
+            eb.List = ContactList.FromText("requiredList.csv", "email\r\nsanta@northpole.com");
+            eb.Attachments = new List<Attachment>()
+            {
+                Attachment.FromText("file1.txt", "content #1"), 
+                Attachment.FromText("file2.txt", "content #2"),
+                Attachment.FromText("file3.txt", "content #3")
+            };
+
+            var xml = eb.ToXml();
+            XDocument xDoc = XDocument.Parse(xml);
+            var orderTag = xDoc.Root.Element("Order");
+            var attachmentsTag = orderTag.Element("Attachments");
+
+            Assert.IsNotNull(attachmentsTag);
+            Assert.IsTrue(attachmentsTag.Elements().Count() == 3);
+
+            var attachmentTag1 = attachmentsTag.Elements().ToList()[0];
+            Assert.IsNotNull(attachmentTag1.Element("AttachmentID"));
+            Assert.IsTrue(attachmentTag1.Element("AttachmentName").Value == "file1.txt");
+            var value = attachmentTag1.Element("AttachmentBinary").Value;
+            Assert.IsTrue(attachmentTag1.Element("AttachmentBinary").Value == Convert.ToBase64String(Encoding.UTF8.GetBytes("content #1")));
+            var attachmentTag2 = attachmentsTag.Elements().ToList()[1];
+            Assert.IsNotNull(attachmentTag2.Element("AttachmentID"));
+            Assert.IsTrue(attachmentTag2.Element("AttachmentName").Value == "file2.txt");
+            Assert.IsTrue(attachmentTag2.Element("AttachmentBinary").Value == Convert.ToBase64String(Encoding.UTF8.GetBytes("content #2")));
+            var attachmentTag3 = attachmentsTag.Elements().ToList()[2];
+            Assert.IsNotNull(attachmentTag3.Element("AttachmentID"));
+            Assert.IsTrue(attachmentTag3.Element("AttachmentName").Value == "file3.txt");
+            Assert.IsTrue(attachmentTag3.Element("AttachmentBinary").Value == Convert.ToBase64String(Encoding.UTF8.GetBytes("content #3")));
+        }
+
+
+        [TestMethod,Ignore]
         public void SerializeTest()
         {
             XmlSerializer serializer = new XmlSerializer(typeof(EmailBroadcast));
@@ -76,6 +240,5 @@ namespace AlertSolutions.APIClient.Test.Broadcasts
             Assert.IsTrue(deserializedMessage.EmailSubject == "test email message");
             var xml = deserializedMessage.ToXml();
         }
-
     }
 }
--- AlertSolutions.APIClient.Test/Broadcasts/VoiceBroadcastTest.cs
+++ AlertSolutions.APIClient.Test/Broadcasts/VoiceBroadcastTest.cs
@@ -2,8 +2,10 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 using System.Xml.Serialization;
 using AlertSolutions.API.Broadcasts;
 using AlertSolutions.API.Documents;
@@ -23,15 +25,166 @@ namespace AlertSolutions.APIClient.Test.Broadcasts
             Assert.IsNotNull(voiceBroadcast);
         }
 
+        // msbuild doesn't seem to handle this attribute, so it fails testfast.bat
+        //[TestMethod, ExpectedException(typeof(FormatException))]
+        //public void BuildXmlWithoutRequiredInput()
+        //{
+        //    var vb = new VoiceBroadcast();
+        //    var xml = vb.ToXml();
+        //}
+
         [TestMethod]
-        public void XmlOutputTest()
+        public void BuildXmlWithRequiredInput()
         {
+            var vb = new VoiceBroadcast();
+            vb.List = ContactList.FromText("requiredList.csv", "phone\r\n5555555555");
+            vb.Documents = new List<Document>()
+            {
+                VoiceDocument.FromText("live01.txt", "this is a live voice call.", VoiceDocumentType.Live),
+            };
+
+            var xml = vb.ToXml();
+            Assert.IsNotNull(xml);
+            XDocument xDoc = XDocument.Parse(xml);
+            Assert.IsNotNull(xDoc);
         }
 
         [TestMethod]
+        public void XmlOutputAllFieldsTest()
+        {
+            var vb = new VoiceBroadcast();
+            vb.BillCode = "BC";
+            vb.ProjectCode = "PC";
+            vb.CallerID = "4015555555";
+            vb.ThrottleType = VoiceBroadcast.VoiceThrottleType.MaximumCalls;
+            vb.ThrottleNumber = 2;
+            vb.List = ContactList.FromText("requiredList.csv", "phone\r\n5555555555");
+            vb.VoiceHeader = "phone";
+            vb.Documents = new List<Document>()
+            {
+                VoiceDocument.FromText("live01.txt", "this is a live voice call.", VoiceDocumentType.Live),
+            };
+
+            var xml = vb.ToXml();
+            Assert.IsNotNull(xml);
+            XDocument xDoc = XDocument.Parse(xml);
+            Assert.IsNotNull(xDoc);
+
+            // make sure the necessary voice fields are there (as per postAPI documentation)
+            VerifyFieldsExist(xDoc);
+            
+            // make sure the necessary VALUES exist in those fields (as per postAPI documentation)
+            var orderTag = xDoc.Root.Elements("Order").FirstOrDefault();
+            Assert.IsTrue(orderTag.FirstAttribute.Value == "VL");
+            Assert.IsTrue(orderTag.Element("Project").Value == "PC");
+            Assert.IsTrue(orderTag.Element("BillCode").Value == "BC");
+            Assert.IsTrue(orderTag.Element("AutoLaunch").Value == "Yes");
+            Assert.IsTrue(DateTime.Parse(orderTag.Element("Date").Value) == DateTime.Today);
+            Assert.IsTrue(DateTime.Parse(orderTag.Element("Time").Value).Minute == DateTime.UtcNow.Minute);
+            // 3 1 0-4 Documentation says StopTime, but I think the name changed since then.
+            Assert.IsNotNull(DateTime.Parse(orderTag.Element("StopDateTime").Value));
+            Assert.IsNotNull(DateTime.Parse(orderTag.Element("RestartTime").Value));
+            Assert.IsTrue(orderTag.Element("Restart").Value == "No");
+            Assert.IsTrue(Enumerable.Range(0, 5).Contains(int.Parse(orderTag.Element("NumberOfRedials").Value)));
+            Assert.IsTrue(Enumerable.Range(0, 3).Contains(int.Parse(orderTag.Element("NumberOfResends").Value)));
+            Assert.IsNotNull(orderTag.Element("CallerID").Value);
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotOne").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotTwo").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotThree").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotFour").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotFive").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotSix").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotSeven").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotEight").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotNine").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotPound").Value));
+            Assert.IsTrue(string.IsNullOrEmpty(orderTag.Element("HotStar").Value));
+            Assert.IsNotNull(string.IsNullOrEmpty(orderTag.Element("ThrType").Value));
+            Assert.IsNotNull(string.IsNullOrEmpty(orderTag.Element("ThrNum").Value));
+
+
+            // TODO : make tests for these conditional values and their output
+            // conditionals
+            Assert.IsNotNull(orderTag.Element("ListID"));
+            Assert.IsNotNull(orderTag.Element("ListName"));
+            Assert.IsNotNull(orderTag.Element("ListBinary"));
+            Assert.IsNotNull(orderTag.Element("VoiceHeader"));
+
+            // documents
+            Assert.IsNotNull(orderTag.Element("Documents"));
+            Assert.IsTrue(orderTag.Element("Documents").Elements().Count() == 1);
+        }
+
+        public void VerifyFieldsExist(XDocument orderXml)
+        {
+            Assert.IsNotNull(orderXml);
+            var orderTag = orderXml.Root.Elements("Order").FirstOrDefault();
+            Assert.IsTrue(orderTag.FirstAttribute.Value == "VL");
+            Assert.IsNotNull(orderTag.Element("Project"));
+            Assert.IsNotNull(orderTag.Element("BillCode"));
+            Assert.IsNotNull(orderTag.Element("AutoLaunch"));
+            Assert.IsNotNull(orderTag.Element("Date"));
+            Assert.IsNotNull(orderTag.Element("Time"));
+            // 3 1 0-4 Documentation says StopTime, but I think the name changed since then.
+            Assert.IsNotNull(orderTag.Element("StopDateTime"));
+            Assert.IsNotNull(orderTag.Element("Restart"));
+            Assert.IsNotNull(orderTag.Element("RestartTime"));
+            Assert.IsNotNull(orderTag.Element("NumberOfRedials"));
+            Assert.IsNotNull(orderTag.Element("NumberOfResends"));
+            Assert.IsNotNull(orderTag.Element("CallerID"));
+            Assert.IsNotNull(orderTag.Element("HotOne"));
+            Assert.IsNotNull(orderTag.Element("HotTwo"));
+            Assert.IsNotNull(orderTag.Element("HotThree"));
+            Assert.IsNotNull(orderTag.Element("HotFour"));
+            Assert.IsNotNull(orderTag.Element("HotFive"));
+            Assert.IsNotNull(orderTag.Element("HotSix"));
+            Assert.IsNotNull(orderTag.Element("HotSeven"));
+            Assert.IsNotNull(orderTag.Element("HotEight"));
+            Assert.IsNotNull(orderTag.Element("HotNine"));
+            Assert.IsNotNull(orderTag.Element("HotPound"));
+            Assert.IsNotNull(orderTag.Element("HotStar"));
+            Assert.IsNotNull(orderTag.Element("ThrType"));
+            Assert.IsNotNull(orderTag.Element("ThrNum"));
+            Assert.IsNotNull(orderTag.Element("ListID"));
+            Assert.IsNotNull(orderTag.Element("ListName"));
+            Assert.IsNotNull(orderTag.Element("ListBinary"));
+            Assert.IsNotNull(orderTag.Element("VoiceHeader"));
+            Assert.IsNotNull(orderTag.Element("Documents"));
+            Assert.IsTrue(orderTag.Element("Documents").Elements().Any());
+        }
+
+        //[TestMethod]
+        //public void XmlOutputVoiceDocumentsTest()
+        //{
+        //    //TODO : copy test similar to EB's attachment test
+        //}
+
+        [TestMethod, Ignore]
         public void SerializeTest()
         {
-        }
+            XmlSerializer serializer = new XmlSerializer(typeof(EmailBroadcast));
+            Order order = new EmailBroadcast()
+            {
+                DisplayName = "jt",
+                EmailSubject = "test email message",
+                SendTimeUTC = DateTime.UtcNow,
+                TextBody = TextBody.FromText("This is a test email")
+            };
 
+            StringBuilder sb = new StringBuilder();
+            TextWriter writer = new StringWriter(sb);
+            serializer.Serialize(writer, order);
+            writer.Close();
+
+            Assert.IsNotNull(sb);
+            Assert.IsTrue(!string.IsNullOrEmpty(sb.ToString()));
+
+            TextReader tr = new StringReader(sb.ToString());
+            object obj = serializer.Deserialize(tr);
+            var deserializedMessage = obj as EmailBroadcast;
+            Assert.IsTrue(deserializedMessage.DisplayName == "jt");
+            Assert.IsTrue(deserializedMessage.EmailSubject == "test email message");
+            var xml = deserializedMessage.ToXml();
+        }
     }
 }

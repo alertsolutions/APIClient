b756fbc5539f152b3528b46a4b72a8fe9fee1630
andrewmaillet <amaillet@blimessaging>
2015-10-15 11:34:28 -0400

    Set default encoding to UTF8 on webclient requests

--- AlertSolutions.APIClient/WebClientProxy.cs
+++ AlertSolutions.APIClient/WebClientProxy.cs
@@ -4,26 +4,28 @@ namespace AlertSolutions.API
 {
     public interface IWebClientProxy
     {
-        string UploadString(string location, string xml, string header = "Content-Type: text/xml");
-        string DownloadString(string location, string header = "Content-Type: text/xml");
+        string UploadString(string location, string xml, string header = "Content-Type: text/xml; charset=UTF-8");
+        string DownloadString(string location, string header = "Content-Type: text/xml; charset=UTF-8");
     }
 
     public class WebClientProxy : IWebClientProxy
     {
 
-        public string UploadString(string location, string xml, string header = "Content-Type: text/xml")
+        public string UploadString(string location, string xml, string header = "Content-Type: text/xml; charset=UTF-8")
         {
             using (var webClient = new WebClient())
             {
+                webClient.Encoding = System.Text.Encoding.UTF8;
                 webClient.Headers.Add(header);
                 return webClient.UploadString(location, xml);
             }
         }
 
-        public string DownloadString(string location, string header = "Content-Type: text/xml")
+        public string DownloadString(string location, string header = "Content-Type: text/xml; charset=UTF-8")
         {
             using (var webClient = new WebClient())
             {
+                webClient.Encoding = System.Text.Encoding.UTF8;
                 webClient.Headers.Add(header);
                 return webClient.DownloadString(location);
             }
